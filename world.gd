8555cc962d14469f0eae35d2b444dc6395b0233e
Sean Yager <contact@seanyager.com>
2024-12-11 20:35:53 -0500

    continue working on multiplayer

--- world.gd
+++ world.gd
@@ -1,6 +1,13 @@
 extends Node
 class_name World
 
+@onready var canvas = $Canvas
+
+const PLAYER = preload("res://actors/player.tscn")
+
+const PORT = 9999
+var enet_peer = ENetMultiplayerPeer.new()
+
 func _ready():
 	pass # Replace with function body.
 
@@ -8,7 +15,22 @@ func _process(delta):
 	pass
 
 func hostButtonPressed():
+	canvas.main_menu.hide()
+	enet_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	add_player(multiplayer.get_unique_id())
 	print('host')
 	
 func joinButtonPressed():
+	canvas.main_menu.hide()
+	enet_peer.create_client("localhost", PORT)
+	multiplayer.multiplayer_peer = enet_peer
+	add_player(multiplayer.get_unique_id())
 	print('join')
+	
+func add_player(peer_id):
+	var player = PLAYER.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
+	print(peer_id)
+	pass
