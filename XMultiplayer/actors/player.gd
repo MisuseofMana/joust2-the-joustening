ffc1f79b45c4aa3f70e5a6c378c9ebe8140e997f
Sean Yager <contact@seanyager.com>
2024-12-13 20:24:31 -0500

    store mutiplayer game in XMultiplayer folder

--- XMultiplayer/actors/player.gd
+++ XMultiplayer/actors/player.gd
@@ -1,13 +1,35 @@
 extends CharacterBody2D
-class_name Player
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 var left =  preload("res://assets/Lance1.png")
 var right = preload("res://assets/Lance2.png")
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+
+var owner_id = 1
+
+func _ready() -> void:
+	owner_id = name.to_int()
+	set_multiplayer_authority(owner_id)
+	
+#	this will eventually move the camera to the players location when brought into multiplayer
+	#if owner_id == multiplayer.get_unique_id():
+		#setup_camera()
+
+func _process(delta):
+	# squash error on client close
+	if multiplayer.multiplayer_peer == null:
+		return
+	# prevent controlling of a player that isn't your own
+	if owner_id != multiplayer.get_unique_id():
+		return
+	
+#	update_camera_position()
 	
 func _physics_process(delta: float) -> void:
+	if owner_id != multiplayer.get_unique_id():
+		return
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -34,6 +56,9 @@ func _physics_process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
+	if owner_id != multiplayer.get_unique_id():
+		return
+		
 	if event.as_text() == "Left":
 		sprite_2d.texture = left
 	elif event.as_text() == "Right": 
--- area_2d.gd
+++ area_2d.gd
@@ -1,5 +1,4 @@
 extends Area2D
 
-
 func _on_input_event(viewport, event, shape_idx):
 	print(event)
